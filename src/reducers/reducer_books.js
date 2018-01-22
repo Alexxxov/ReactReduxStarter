e0d783ff55b4bfb5e073b505b19f6680cc3f2357
Alexander <aav94@yandex.ru>
2018-01-23 00:50:19 +0300

    added active creator binding

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,7 @@
 export default function () {
   return [
     { title: 'Javascript: The Good Parts'},
-    { title: 'Spring Boot on Action'},
+    { title: 'Spring Boot in Action'},
     { title: 'Java EE 8'},
     { title: 'The Final Diagnosis'}
   ]
